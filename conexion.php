247cdbe36a1f369a7ca99f5098ac5b1c5a93acf4
Camilo Rodriguez <rodriguez.camilo@gmail.com>
2015-10-14 14:28:29 -0500

    se incluye archivo php de conexión a la base

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+$user="root";
+$pass="root";
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=pensum', $user, $pass);
+    foreach($dbh->query('SELECT * from NOTAS') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
